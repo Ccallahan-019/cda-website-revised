6a77ec4f752490f36e916766911b927a85830239
Christian Callahan <callahan.0019@gmail.com>
2025-11-21 17:53:47 -0500

    add protocol in production

--- src/graphql/apolloClient.ts
+++ src/graphql/apolloClient.ts
@@ -1,22 +1,24 @@
-import { HttpLink } from "@apollo/client";
+import { HttpLink } from '@apollo/client'
 import {
   registerApolloClient,
   ApolloClient,
   InMemoryCache,
-} from "@apollo/client-integration-nextjs";
-import fetch from "cross-fetch";
+} from '@apollo/client-integration-nextjs'
+import fetch from 'cross-fetch'
 
 export function getApolloServerClient(token?: string) {
   const { getClient } = registerApolloClient(() => {
     return new ApolloClient({
       cache: new InMemoryCache(),
       link: new HttpLink({
-        uri: `${process.env.NEXT_PUBLIC_SERVER_URL || process.env.VERCEL_PROJECT_PRODUCTION_URL}/api/graphql`,
+        uri: process.env.NEXT_PUBLIC_SERVER_URL
+          ? `${process.env.NEXT_PUBLIC_SERVER_URL}/api/graphql`
+          : `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}/api/graphql`,
         fetch,
         headers: token ? { Authorization: `JWT ${token}` } : {},
       }),
-    });
-  });
+    })
+  })
 
-  return getClient();
+  return getClient()
 }
