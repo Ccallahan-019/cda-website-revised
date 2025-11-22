ac59b445976f9f10da0aa18974ec252e9440860b
Christian Callahan <callahan.0019@gmail.com>
2025-11-22 07:00:49 -0500

    apply removal of nested docs plugin to db schema

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20251121_201841 from './20251121_201841';
+import * as migration_20251122_115837 from './20251122_115837';
 
 export const migrations = [
   {
     up: migration_20251121_201841.up,
     down: migration_20251121_201841.down,
-    name: '20251121_201841'
+    name: '20251121_201841',
+  },
+  {
+    up: migration_20251122_115837.up,
+    down: migration_20251122_115837.down,
+    name: '20251122_115837'
   },
 ];
