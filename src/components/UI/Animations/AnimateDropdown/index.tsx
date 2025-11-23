78cc7c0c1258d7ef0b0c400fd962f0105e9e3241
Christian Callahan <callahan.0019@gmail.com>
2025-11-23 04:22:16 -0500

    implement AnimatePresence to avoid reserved space in DOM

--- src/components/UI/Animations/AnimateDropdown/index.tsx
+++ src/components/UI/Animations/AnimateDropdown/index.tsx
@@ -1,17 +1,26 @@
-import { motion } from 'framer-motion'
-import React from 'react'
+'use client'
+
+import { Box } from '@radix-ui/themes'
+import { AnimatePresence, motion } from 'framer-motion'
 
 export const AnimateDropdown: React.FC<{
   children: React.ReactNode
   isExpanded: boolean
 }> = ({ children, isExpanded }) => {
   return (
-    <motion.div
-      style={{ overflowY: 'hidden' }}
-      initial={false}
-      animate={{ height: isExpanded ? 'auto' : 0 }}
-    >
-      {children}
-    </motion.div>
+    <AnimatePresence initial={false}>
+      {isExpanded && (
+        <motion.div
+          key="dropdown"
+          initial={{ height: 0, opacity: 0 }}
+          animate={{ height: 'auto', opacity: 1 }}
+          exit={{ height: 0, opacity: 0 }}
+          transition={{ duration: 0.3 }}
+          style={{ overflow: 'hidden' }}
+        >
+          <Box>{children}</Box>
+        </motion.div>
+      )}
+    </AnimatePresence>
   )
 }
