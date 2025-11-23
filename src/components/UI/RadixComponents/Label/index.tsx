e5f14faa6d74e4b6fb5283779ad854e32c72d70a
Christian Callahan <callahan.0019@gmail.com>
2025-11-23 03:44:35 -0500

    add padding

--- src/components/UI/RadixComponents/Label/index.tsx
+++ src/components/UI/RadixComponents/Label/index.tsx
@@ -1,29 +1,31 @@
 'use client'
 
-import { Flex, Text, TextProps } from '@radix-ui/themes'
+import { Box, Flex, Text, TextProps } from '@radix-ui/themes'
 
 const Label: React.FC<
   { label: string; required?: boolean; ref?: React.Ref<HTMLLabelElement> } & TextProps
 > = ({ label, required, ref, size, wrap, weight, ...props }) => {
   return (
-    <Text
-      as="label"
-      ref={ref}
-      size={size ? size : { initial: '2', xs: '3' }}
-      wrap={wrap ? wrap : 'pretty'}
-      weight={weight ? weight : 'bold'}
-      {...props}
-    >
-      <Flex as="span" gap="1">
-        {required && (
-          <Text as="span" color="red">
-            {'*'}
-            <span className="sr-only">required</span>
-          </Text>
-        )}
-        {label}
-      </Flex>
-    </Text>
+    <Box py="1">
+      <Text
+        as="label"
+        ref={ref}
+        size={size ? size : { initial: '2', xs: '3' }}
+        wrap={wrap ? wrap : 'pretty'}
+        weight={weight ? weight : 'bold'}
+        {...props}
+      >
+        <Flex as="span" gap="1">
+          {required && (
+            <Text as="span" color="red">
+              {'*'}
+              <span className="sr-only">required</span>
+            </Text>
+          )}
+          {label}
+        </Flex>
+      </Text>
+    </Box>
   )
 }
 
